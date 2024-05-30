5a81ee7f46f55a54b338762e3dcaa321b827069f
dextro-guy <141754185+dextro-guy@users.noreply.github.com>
2024-05-30 23:24:22 +0530

    created-nav-and-footer-tags

---
+++ components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <footer>
+        <p>Get me a Chai - Fund your project with chai</p>
+      </footer>
+    </div>
+  )
+}
+
+export default Footer
